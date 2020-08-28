395b7480b537c138f349e1cb5186f55bfa751b65
kiily <miguelmarin95@hotmail.com>
2020-08-29 01:52:33 +0200

    feat(tailwind): add basic tailwind setup

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,4 @@
-import './index.scss';
+import './tailwind.output.css';
 
 import React from 'react';
 import ReactDOM from 'react-dom';
@@ -16,4 +16,4 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.register();
+serviceWorker.unregister();
